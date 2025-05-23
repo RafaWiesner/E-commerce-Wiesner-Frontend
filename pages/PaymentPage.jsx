6a4f9f96bf30bc27590e1e9c142499b730792c99
Rafael Wiesner <seu-email@exemplo.com>
2025-05-23 10:29:51 -0300

    Ajuste

--- pages/PaymentPage.jsx
+++ pages/PaymentPage.jsx
@@ -130,7 +130,7 @@ export default function PaymentPage() {
           name: item.name,
           quantity: item.quantity,
           price: parseFloat(item.price),
-          ProductImage: getImageUrl(item.image)
+          productImage: getImageUrl(item.image)
         })),
         shippingAddress: {
           address: formData.address,
