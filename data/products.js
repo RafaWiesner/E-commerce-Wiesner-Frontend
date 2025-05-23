d00e17adbb483e8e417baa3c2ea201315839131d
Rafael Wiesner <seu-email@exemplo.com>
2025-05-23 09:57:07 -0300

    Correção exibição imagens nos pedidos

--- data/products.js
+++ data/products.js
@@ -1,6 +1,3 @@
-const relogio1 = '/assets/relogio1.jpeg';
-const relogio2 = '/assets/relogio2.jpg';
-const relogio3 = '/assets/relogio3.webp';
 
 export const products = [
   {
@@ -8,20 +5,20 @@ export const products = [
     name: "Elegance Vintage",
     description: "O design minimalista faz dele a escolha ideal para quem aprecia um estilo clássico e refinado.",
     price: 199.90,
-    image: '/assets/relogio1.jpeg'
+    image: "/assets/relogio1.jpeg"
   },
   {
     id: 2,
     name: "Tech Chrono X",
     description: "O design combina sofisticação e praticidade, ideal para quem busca estilo clássico e funcionalidade no dia a dia.",
     price: 299.90,
-    image: '/assets/relogio2.jpg'
+    image: "/assets/relogio2.jpg"
   },
   {
     id: 3,
     name: "Urban Tech Pro",
     description: "Um ícone de elegância com design sofisticado. É a escolha perfeita para quem busca estilo e funcionalidade em um acessório.",
     price: 399.90,
-    image: '/assets/relogio3.webp'
+    image: "/assets/relogio3.webp"
   }
 ]; 
